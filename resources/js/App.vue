f97f454ebd6a815e7fd6796469154132d68361b1
hakobjanyanArtyom <dark.artyom1@mail.ru>
2019-09-29 21:48:26 +0400

    change

---
+++ resources/js/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="wrapp" v-if="$auth.ready()">
+        <Header></Header>
+        <router-view class="main-router-view"></router-view>
+        <Footer></Footer>
+
+        <div v-if="!$auth.ready()">
+            Loading...
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import axios from 'axios';
+    import Header from './components/includes/Header'
+    import Footer from './components/includes/Footer'
+
+    export default {
+        components: {
+            Header,
+            Footer
+        },
+        methods: {
+            test(){
+                axios.post('/auth/test')
+
+                .then(response => {
+                    console.log(response)
+                })
+                .catch(error => {
+                    console.log(error)
+
+                });
+            }
+        }
+    }
+</script>
+<style>
+    .main-router-view{
+        max-width: 1280px;
+        margin:40px auto;
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
---
+++ resources/js/components/includes/Footer.vue
@@ -0,0 +1,78 @@
+<template>
+    <footer>
+        <div class="footer-top">
+            <ul>
+                <li><router-link to="/catalog" > Catalog </router-link></li>
+                <li><router-link to="/privacy" > Privacy Policy </router-link></li>
+                <li><router-link to="/terms" > Terms of Use </router-link></li>
+                <li><router-link to="/cpr-protection" > Copyright Protection </router-link></li>
+                <li><router-link to="/support" > Support </router-link></li>
+                <li><router-link to="/about" >About AliMarket</router-link></li>
+                <li><router-link to="/seller" > Become a seller </router-link></li>
+            </ul>
+        </div>
+
+
+        <div class="footer-bottom">
+            <p>© 2019 SIA AliMarket (Armenia)</p>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Footer already.')
+        }
+    }
+</script>
+
+
+<style>
+
+    footer{
+        background: white;
+    }
+    .footer-top{
+        max-width: 1280px;
+        min-height: 52px;
+        margin: auto;
+        border-bottom: 1px solid #f2f2f2;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+
+    .footer-bottom{
+        max-width: 1280px;
+        min-height: 52px;
+        margin: auto;
+    }
+
+    .footer-top ul{
+        display: flex;
+        margin-bottom: 0;
+    }
+
+    .footer-top ul li{
+        list-style: none;
+        padding-right: 16px;
+    }
+
+    .footer-top ul li a{
+        color:#9697a1;
+    }
+
+    .footer-bottom{
+        display: flex;
+        align-items: center;
+    }
+
+    .footer-bottom p{
+        margin-bottom: 0!important;
+        color:#9697a1;
+    }
+
+</style>
